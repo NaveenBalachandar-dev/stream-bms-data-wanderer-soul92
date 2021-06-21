f707dffd2ccffc57063ababf08ebd015b975943f
NaveenBalachandar-dev <77613377+NaveenBalachandar-dev@users.noreply.github.com>
2021-06-21 22:14:48 +0530

    Update and rename receiverBmsProj.c to BMS_Receiver.c

---
+++ BMS_Receiver/BMS_Receiver.c
@@ -0,0 +1,20 @@
+/*--------------------------------------------------------------------------------
+---    \file       BMS_Receiver.c
+---   \author      Naveen Balachandar (RBEI/ECK3) 
+---
+---     \brief     proj file to recive and process stream data
+------------------------------------------------------------------------------------*/
+/*------ standard includes -------*/
+#include <stdio.h>
+#include <stdlib.h>
+
+/*------ Project includes -------*/
+//#include "BMS_Receiver.h"
+
+
+int  main()
+{
+  printf("Receiver data processing")
+  return 0;
+}
+
